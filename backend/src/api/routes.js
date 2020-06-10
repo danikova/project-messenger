9060e370cabe1b2bebf61a9088275db0ce11ed11
danikova <kovacs.daniel.9509@gmail.com>
2020-06-10 10:05:28 +0200

    listing all api endpoints

--- backend/src/api/routes.js
+++ backend/src/api/routes.js
@@ -1,7 +1,10 @@
+const express = require('express');
 const roomRoutes = require('./rooms/routes');
 const userRoutes = require('./users/routes');
 
-module.exports = function (router) {
-    roomRoutes(router);
-    userRoutes(router);
+module.exports = function () {
+    const apiRouter = express.Router();
+    roomRoutes(apiRouter);
+    userRoutes(apiRouter);
+    return apiRouter;
 };
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,20 +2,18 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const listEndpoints = require('express-list-endpoints');
 
 const setup = require('./config/setup');
 const databaseSetup = require('./config/database');
 const middlewares = require('./middlewares');
-const apiRoutes = require('./api/routes');
-
-const router = express.Router();
+const setApiRoutes = require('./api/routes');
 
 databaseSetup();
 middlewares(app);
 
-app.use('/api',router);
-
-apiRoutes(router);
+const apiRouter = setApiRoutes();
+app.use('/api', apiRouter);
 
 server.listen(setup.PORT, () => {
     console.log(`Server is running on ${setup.PORT} port.`);
@@ -23,4 +21,8 @@ server.listen(setup.PORT, () => {
 
 io.on('connection', () => {
     /* … */
+});
+
+app.get('/', function (req, res, next) {
+    res.json(listEndpoints(app)).status(200);
 });
