72510d83debe28dfc51c381fd7250f9f4e8db46a
danikova <kovacs.daniel.9509@gmail.com>
2020-06-13 12:02:32 +0200

    flag for authentication

--- backend/src/api/routes.js
+++ backend/src/api/routes.js
@@ -2,10 +2,11 @@ const express = require('express');
 const roomRoutes = require('./rooms/routes');
 const userRoutes = require('./users/routes');
 const tokenAuth = require('../middlewares/token.auth');
+const config = require('config');
 
 module.exports = function () {
     const apiRouter = express.Router();
-    apiRouter.use(tokenAuth);
+    if (config.get('authentication.enabled')) apiRouter.use(tokenAuth);
     roomRoutes(apiRouter);
     userRoutes(apiRouter);
     return apiRouter;
