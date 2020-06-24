6908502b4d9443d66183fbacf2ff19b35ddca427
danikova <kovacs.daniel.9509@gmail.com>
2020-06-24 20:43:38 +0200

    small message schema change

--- backend/src/api/messages/schema.js
+++ backend/src/api/messages/schema.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    user: { type: Schema.Types.ObjectId, ref: 'Users' },
+    user: { type: Schema.Types.ObjectId, ref: 'Users', default: null },
     message: { type: String },
     sent: { type: Date, default: Date.now },
 });
