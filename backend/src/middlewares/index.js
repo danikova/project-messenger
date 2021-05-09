1137c15915983c290ef663d28341498bf80a3f10
danikova <kovacs.daniel.9509@gmail.com>
2021-05-09 11:03:38 +0200

    middleware cleanup

--- backend/src/middlewares/index.js
+++ backend/src/middlewares/index.js
@@ -1,29 +1,9 @@
-const log = require('morgan')('dev');
 const bodyParser = require('body-parser');
-const winston = require('winston')
-const expressWinston = require('express-winston');
-
 const bodyParserJSON = bodyParser.json();
 const bodyParserURLEncoded = bodyParser.urlencoded({ extended: true });
 
 module.exports = function (app) {
-    app.use(log);
-    app.use(
-        expressWinston.logger({
-            transports: [new winston.transports.Console()],
-            format: winston.format.combine(
-                winston.format.colorize(),
-                winston.format.json(),
-            ),
-            meta: true,
-            msg: 'HTTP {{req.method}} {{req.url}}',
-            expressFormat: true,
-            colorize: false,
-            ignoreRoute: function (req, res) {
-                return false;
-            },
-        }),
-    );
+    app.use(require('morgan')('dev'));
     app.use(bodyParserJSON);
     app.use(bodyParserURLEncoded);
 };
