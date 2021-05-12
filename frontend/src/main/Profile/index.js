2e91cda116c95a9ff8355801832973b04c5f8ffc
danikova <kovacs.daniel.9509@gmail.com>
2021-05-12 12:54:16 +0200

    dummy profile added

---
+++ frontend/src/main/Profile/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export class Profile extends React.Component {
+    render() {
+        return (
+            <div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+                <div>Profile</div>
+            </div>
+        );
+    }
+}
+
+export default Profile;
