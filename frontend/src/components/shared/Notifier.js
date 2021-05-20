7185ae32d3b0edc452da132ad00c8aaa2de4fb0b
danikova <kovacs.daniel.9509@gmail.com>
2021-05-20 19:02:43 +0200

    notifier component

---
+++ frontend/src/components/shared/Notifier.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useSnackbar } from 'notistack';
+import { removeSnackbar } from '../../store/actions/notifications.action';
+import { useIntl } from 'react-intl';
+
+let displayed = [];
+
+const Notifier = () => {
+    const notifications = useSelector((store) => store.notifications || []);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const intl = useIntl();
+
+    const storeDisplayed = (id) => {
+        displayed = [...displayed, id];
+    };
+
+    const removeDisplayed = (id) => {
+        displayed = [...displayed.filter((key) => id !== key)];
+    };
+
+    React.useEffect(() => {
+        notifications.forEach(
+            ({
+                key,
+                message,
+                options = {},
+                dismissed = false,
+                templateName = null,
+                templateVariables = {},
+            }) => {
+                if (dismissed) {
+                    closeSnackbar(key);
+                    return;
+                }
+
+                if (displayed.includes(key)) return;
+
+                enqueueSnackbar(message, {
+                    key,
+                    ...options,
+                    onClose: (event, reason, myKey) => {
+                        if (options.onClose) {
+                            options.onClose(event, reason, myKey);
+                        }
+                    },
+                    onExited: (event, myKey) => {
+                        removeSnackbar(myKey);
+                        removeDisplayed(myKey);
+                    },
+                });
+                storeDisplayed(key);
+            },
+        );
+    }, [notifications, closeSnackbar, enqueueSnackbar]);
+
+    return null;
+};
+
+export default Notifier;
